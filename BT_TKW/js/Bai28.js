3832f4c0430b6c646b9de69b0f8df0d501da1bf9
Nguyễn Văn Hải Long - 60136035 <63218600+longzero2000@users.noreply.github.com>
2020-08-10 18:37:14 +0700

    update
    update

--- BT_TKW/js/Bai28.js
+++ BT_TKW/js/Bai28.js
@@ -1,48 +1,9 @@
 window.onload=function myFuntion(){
-	//Cong
-	document.getElementById("cong").onclick=
-	function PhepCong(){
-		var m=document.getElementById("tinh");
-		var x=parseInt(m.x.value);
-		var y=parseInt(m.y.value);
-		var kq=x+y;
-		m.kq.value=kq;
-	}
-	//Tru
-	document.getElementById("tru").onclick=
-	function PhepTru(){
-		var m=document.getElementById("tinh");
-		var x=parseInt(m.x.value);
-		var y=parseInt(m.y.value);
-		var kq=x-y;
-		m.kq.value=kq;
-	}
-	//Nhan
-	document.getElementById("nhan").onclick=
-	function PhepNhan(){
-		var m=document.getElementById("tinh");
-		var x=parseInt(m.x.value);
-		var y=parseInt(m.y.value);
-		var kq=x*y;
-		m.kq.value=kq;
-	}
-	//Chia
-	document.getElementById("chia").onclick=
-	function PhepChia(){
-		var m=document.getElementById("tinh");
-		var x=parseInt(m.x.value);
-		var y=parseInt(m.y.value);
-		var kq=x/y;
-		m.kq.value=kq;
-	}
-	//Mu
-	document.getElementById("mu").onclick=
-	function TinhMu(){
-		var m=document.getElementById("tinh");
-		var x=parseInt(m.x.value);
-		var y=parseInt(m.y.value);
-		var kq=Math.pow(x,y);
-		m.kq.value=kq;
+	var enter=document.getElementById("Enter");
+	
+	enter.onclick=function(){
+		var url=document.getElementById("url");
+		window.open(url.value);
 	}
 	
 }
